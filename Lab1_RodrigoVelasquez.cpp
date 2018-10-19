13c72e57a69c26afd85f2ac99f0487965f2ba663
Rodrigo Velasquez <rodrix183@hotmail.com>
2018-10-19 13:23:52 -0600

    Menu

--- Lab1_RodrigoVelasquez.cpp
+++ Lab1_RodrigoVelasquez.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
-
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main(){
+	int resp = 0;
+	do{
+		cout<<"-------------------------Menu--------------"<<endl
+		<<"Triforce"<<endl
+		<<"Logaritmos"<<endl
+		<<"F(x)"<<endl;
+		cin>>resp;
+		switch(resp){
+		case 1:{
 
-return 0;
+		}
+		break;
+
+		case 2:{
+
+		}
+		break;
+
+		case 3:{
+		
+		}
+		break;
+
+		default:{
+			cout<<"Saliendo del Programa..."<<endl;
+			resp = 4;
+		}
+		break;
+		}
+	}while(resp!=4);
+	return 0;
+
 }
